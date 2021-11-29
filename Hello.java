3df6c64df1310c61740d14c5ce9cf4ebfd53b031
KJEzorro <gndlg91@gmail.com>
2021-11-29 17:58:41 +0900

    .

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world!1");
-        System.out.printn("Hello world!2");
+        System.out.println("Hello world!2");
         System.out.println("Hello world!3");
 
         
