473d96afacdace314f16bea5cabba649d13ceee9
KJEzorro <gndlg91@gmail.com>
2021-12-06 18:12:26 +0900

    .

--- java-lang/app/src/main/java/com/eomcs/lang/ex04/Exam0951.java
+++ java-lang/app/src/main/java/com/eomcs/lang/ex04/Exam0951.java
@@ -5,21 +5,33 @@ package com.eomcs.lang.ex04;
 public class Exam0951 {
   public static void main(String[] args) {
 
-    boolean v1 = Boolean.valueOf("true"); // 문자열 ==> boolean
+    byte b = Byte.valueOf("100");
+    short s = Short.valueOf("32767");
+    int i1 = Integer.valueOf("2122223333");
+    int i2 = Integer.parseInt("2122223333");
+    long l = Long.valueOf("9221111222233334444");
+    float f1 = Float.valueOf("3.14f");  // 문자열 ==> float
+    float f2 = Float.parseFloat("3.14f");  // 문자열 ==> float
+    double d1 = Double.valueOf("9876.54321");
+    double d2 = Double.parseDouble("9876.54321");
+    boolean bool1 = Boolean.valueOf("1"); // 문자열 ==> boolean
+    boolean bool2 = Boolean.parseBoolean("TRUE"); // 문자열 ==> boolean
+    char c = "가".charAt(0);
 
-    char v2 = "가".charAt(0);  // 문자열 ==> char
 
-    int v3 = Integer.valueOf("123");  // 문자열 ==> int
-    int v33 = Integer.parseInt("123");  // 문자열 ==> int
 
-    float v4 = Float.valueOf("3.14f");  // 문자열 ==> float
-    float v44 = Float.parseFloat("3.14f");  // 문자열 ==> float
 
-    System.out.println(v1);
-    System.out.println(v2);
-    System.out.println(v3 + 1);
-    System.out.println(v33);
-    System.out.println(v4);
-    System.out.println(v44);
+    System.out.println(b);
+    System.out.println(s);
+    System.out.println(i1);
+    System.out.println(i2);
+    System.out.println(l);
+    System.out.println(f1);
+    System.out.println(f2);
+    System.out.println(d1);
+    System.out.println(d2);
+    System.out.println(bool1);
+    System.out.println(bool2);
+    System.out.println(c);
   }
 }
