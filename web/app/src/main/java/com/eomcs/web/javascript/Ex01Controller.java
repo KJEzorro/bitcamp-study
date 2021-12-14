6ff6db73c6d466c5a4b55a2b628b2f5d63e79220
KJEzorro <gndlg91@gmail.com>
2021-12-14 23:48:18 +0900

    .

---
+++ web/app/src/main/java/com/eomcs/web/javascript/Ex01Controller.java
@@ -0,0 +1,31 @@
+package com.eomcs.web.javascript;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Ex01Controller {
+
+  @RequestMapping("/javascript/ex01/exam05_1")
+  public Object exam05_1() throws Exception{
+
+    //    Thread.sleep(10000); // 10초 동안 기다렸다가 작업을 재개한다.
+    return "window.alert('okok')";  // 자바 스크립트 코드 조각을 리턴한다.
+
+  }
+
+  @RequestMapping("/javascript/ex01/exam05_2")
+  public Object exam05_2() {
+    return "window.alert('nono')";  // 자바 스크립트 코드 조각을 리턴한다.
+
+  }
+
+  @RequestMapping("/javascript/ex01/exam05_x")
+  public Object exam05_x() throws Exception{
+    Thread.sleep(10000);
+    return "50,000";  // 데이터를 리턴한다.
+
+  }
+
+}
