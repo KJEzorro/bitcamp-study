46d71f99d7b593e3387cf93f293a1a4f91569bbc
KJEzorro <gndlg91@gmail.com>
2021-12-15 11:36:44 +0900

    .

--- java-lang-boot/app/src/main/java/com/eomcs/study/App.java
+++ java-lang-boot/app/src/main/java/com/eomcs/study/App.java
@@ -1,10 +1,14 @@
 package com.eomcs.study;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@SpringBootApplication
+@RestController
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.eomcs.study2", "com.eomcs.study"}) // basePackages 대신에 value도 가능
 public class App {
 
   public static void main(String[] args) {
