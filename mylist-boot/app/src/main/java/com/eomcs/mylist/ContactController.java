2daa63f268cf9138099f640b5b3c40233fd016fb
KJEzorro <gndlg91@gmail.com>
2021-12-16 17:40:54 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
@@ -19,58 +19,87 @@ public class contactController {
     }
     return arr;  // 복사한 항목들을 담고있는 새 배열을 리턴한다.
   }
+
+
   @RequestMapping("/contact/add")
   public Object add(String name, String email, String tel, String company) {
-    String contact = name + "," + email + "," + tel + "," + company;
-    contacts[size++] = contact;
+    contacts[size++] = createCSV(name, email, tel, company);
     return size;
 
   }
 
+
   @RequestMapping("/contact/get")
   public Object get(String email) {
-    for (int i=0; i < size; i++) {
-      // 학습용
-      //      String contact = contacts[i]; // 예) "u1,u1@test.com,1111,비트캠프"
-      //      String[] values = contact.split(","); // 예) {"u1","u1@test.com","1111","비트캠프"}
-      //      if (values[1].equals(email)) { // 예) "u1@test.com"
-      //        return contacts[i];
-      //      }
-
-
-      // 실전
-      if (contacts[i].split(",")[1].equals(email)) {
-        return contacts[i];
-      }
-    }
-    return "";
+    int index = indexOf(email);
+    if (index == -1) {
+      return "";
+    } 
+    return contacts[index];
   }
+
+
 
   @RequestMapping("/contact/update")
   public Object update(String name, String email, String tel, String company) {
-    String contact = name + "," + email + "," + tel + "," + company;
-    for (int i=0; i < size; i++) {
-      if (contacts[i].split(",")[1].equals(email)) {
-        contacts[i] = contact; 
-        return 1;
-      }
+    int index = indexOf(email);
+    if (index == -1) { 
+      return 0; 
     }
-    return 0;
+    contacts[index] = createCSV(name, email, tel, company);
+    return 1;
   }
+
 
   @RequestMapping("/contact/delete")
   public Object delete(String email) {
+    int index = indexOf(email);
+    if (index == -1) {
+      return 0;
+    }
+    remove(index);
+    return 1;
+  }
+
+
+
+
+  // 기능: 입력 받은 파라미터 값을 가지고 CSV형식으로 문자열을 만들어 준다.
+  String createCSV(String name, String email, String tel, String company) {
+    return name + "," + email + "," + tel + "," + company;
+  }
+
+
+  // 기능: 이메일로 연락처 정보를 찾는다.
+  //       찾은 연락처의 배열 인덱스를 리턴한다.
+  int indexOf(String email) {
     for (int i=0; i < size; i++) {
       if (contacts[i].split(",")[1].equals(email)) {
-
-        // 현재 위치의 다음 항목에서 배열 끝까지 반복하며 앞으로 값을 당겨온다.
-        for (int j= i+1; j < size; j++) {
-          contacts[j-1] = contacts[j];
-        }
-        size--;
-        return 1;
+        return i;
       }
     }
-    return 0;
+    return -1;
+  }
+
+  // 기능: 배열에서 지정한 항목을 삭제한다.
+  String remove(int index) {
+    String old = contacts[index];
+    for (int i= index+1; i < size; ++i) {
+      contacts[i-1] = contacts[i];
+    }
+    size--;
+    return old;
+
   }
 }
+
+
+
+
+
+
+
+
+
+
+
