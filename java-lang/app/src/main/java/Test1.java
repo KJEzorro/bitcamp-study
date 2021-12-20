b8a65ce29f13fd6062382026fc39c78ff80ad0c3
KJEzorro <gndlg91@gmail.com>
2021-12-20 14:57:46 +0900

    .

--- java-lang/app/src/main/java/Test1.java
+++ java-lang/app/src/main/java/Test1.java
@@ -2,15 +2,6 @@
 public class Test1 {
 
   public static void main(String[] args) {
-    int i = 20;
-    float i2 = 3.3f;
-    int s;
-
-    s = i + (int)i2;
-    System.out.println(s);
-    System.out.println(s);
-
-
 
   }
 
