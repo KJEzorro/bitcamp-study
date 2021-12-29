ff210b974dc3652f5f342d4b430f7f9cef1115a9
KJEzorro <gndlg91@gmail.com>
2021-12-29 17:19:37 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/BoardController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/BoardController.java
@@ -8,50 +8,58 @@ import org.springframework.web.bind.annotation.RestController;
 // 이 표시(애노테이션)가 붙어 있어야만 스프링부트가 이 클래스를 인식한다.
 public class BoardController {
 
+  // Board 객체 목록을 저장할 메모리를 준비한다.
+  ArrayList boardList = new ArrayList();
+
+
 
   @RequestMapping("/board/list")
   public Object list() {
-    return ArrayList3.toArray();
+    return ArrayList.toArray(boardList);
   }
 
 
   @RequestMapping("/board/add")
   public Object add(Board board) {
+
     board.setCreatedDate(new java.sql.Date(System.currentTimeMillis()));
-    ArrayList3.add(board);
-    return ArrayList3.size;
+    ArrayList.add(boardList, board);
+    return boardList.size;
   }
 
 
   @RequestMapping("/board/get")
   public Object get(int index) {
-    if (index == -1 || index >= ArrayList3.size) {
+    if (index == -1 || index >= boardList.size) {
       return "";
     }
-    ((Board)ArrayList3.list[index]).viewCount++;
-    return ArrayList3.list[index];
+    Board board = (Board)boardList.list[index];
+    board.viewCount++;
+    return boardList.list[index];
   }
 
 
 
   @RequestMapping("/board/update")
   public Object update(int index, Board board) {
-    if (index == -1 || index >= ArrayList3.size) {
-      return "";
+    if (index == -1 || index >= boardList.size) {
+      return 0;
     }
-    Board old = (Board)ArrayList3.list[index];
+
+
+    Board old = (Board)boardList.list[index];
     board.viewCount = old.viewCount;
     board.createdDate = old.createdDate;
-    return ArrayList3.set(index, board) == null ? 0 : 1;
+    return ArrayList.set(boardList, index, board) == null ? 0 : 1;
   }
 
 
   @RequestMapping("/board/delete")
   public Object delete(int index) {
-    //    if (index == -1 || index >= ArrayList3.size) {
-    //      return "";
-    //    }
-    return ArrayList3.remove(index) == null ? 0 : 1; 
+    if (index == -1 || index >= boardList.size) {
+      return "";
+    }
+    return ArrayList.remove(boardList, index) == null ? 0 : 1; 
   }
 
 }
