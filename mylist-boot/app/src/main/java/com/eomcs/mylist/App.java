5e8c81581727bea64f40a6e674847dc18bb00cd5
KJEzorro <gndlg91@gmail.com>
2022-01-07 15:06:14 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
@@ -4,7 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +14,7 @@ public class App {
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
   }
-  @Bean
+  //@Bean
   public CommandLineRunner commandLineRunner(ApplicationContext beanContainer) {
     return args -> {
 
--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
@@ -1,5 +1,6 @@
 package com.eomcs.mylist.controller;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,39 @@ public class ContactController {
 
   ArrayList contactList = new ArrayList();
 
-  public ContactController() {
+  public ContactController()throws Exception {
     System.out.println("ContactController() 호출됨");
+
+    FileReader in = new FileReader("contacts.csv");
+    StringBuilder buf = new StringBuilder();
+    int c;
+
+    while (true) {
+      c = in.read(); // 파일에서 한 문자를 읽는다.
+
+      if (c == -1) { // 더이상 읽을 문자가 없다면 반복문을 종료한다.
+        break;
+      }
+      if (c == '\n') {  // 만약 읽은 문자가 줄바꿈 명령이라면, 지금까지 읽은 CSV 데이터를 분석하여 Contact 객체에 담는다.
+
+        String csvStr = buf.toString(); // 예) "홍길동,hong@test.com,010-2222-3333,비트캠프"
+        String[] values = csvStr.split(","); // 예) ["홍길동", "hong@test.com","010-2222-3333","비트캠프"]
+
+        Contact contact = new Contact(); // 파일에서 읽은 데이터를 담을 객체를 준비한다.
+        contact.setName(values[0]); // 배열에 들어 있는 각 항목을 객체의 필드에 저장한다.
+        contact.setEmail(values[1]);
+        contact.setTel(values[2]);
+        contact.setCompany(values[3]);
+
+
+        contactList.add(contact);// 데이터를 담은 객체를 목록에 추가한다.
+        buf.setLength(0); // 다음 데이터를 읽기 위해 버퍼를 초기화 시킨다.
+
+      } else {
+        buf.append((char) c); // 문자를 읽을 때마다 버퍼에 임시 보관한다.
+      }
+    }
+    in.close();
   }
 
 
