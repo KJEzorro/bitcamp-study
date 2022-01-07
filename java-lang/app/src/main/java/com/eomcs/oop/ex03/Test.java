839892c0a8f490ddf8250e3bdceeeb3be043df97
KJEzorro <gndlg91@gmail.com>
2022-01-07 09:33:23 +0900

    .

--- java-lang/app/src/main/java/com/eomcs/oop/ex03/Test.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex03/Test.java
@@ -2,18 +2,12 @@ package com.eomcs.oop.ex03;
 
 
 class Test {
-  class TTT {
-    int a = 10;
-  }
 
 
 
 
   public static void main(String[] args) {
-    Test a = new Test();
-    TTT t = a.new TTT();
-
-    System.out.println(t.a);
+    System.out.println(Math.class);
   }
 
 }
--- mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
@@ -1,7 +1,10 @@
 package com.eomcs.mylist;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +15,21 @@ public class App {
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
   }
+  @Bean
+  public CommandLineRunner commandLineRunner(ApplicationContext beanContainer) {
+    return args -> {
+
+      System.out.println("빈 컨테이너가 생성한 객체(빈 컨테이너에 들어 있는 객체):");
+
+      String[] beanNames = beanContainer.getBeanDefinitionNames();
+      for (int i = 0; i < beanNames.length; i++) {
+        Object bean = beanContainer.getBean(beanNames[i]);
+        System.out.printf("---->%03d: %s%n", i + 1, bean.getClass().getName());
+      }
+    };
+
+  }
+
   @RequestMapping("/hello")
   String hello() {
     return "Hello World!";
