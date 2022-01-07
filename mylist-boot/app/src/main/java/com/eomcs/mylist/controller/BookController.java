0366c87bbfb574a6b943dcb6bd95647db3123aff
KJEzorro <gndlg91@gmail.com>
2022-01-07 09:34:25 +0900

    .

---
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BookController.java
@@ -0,0 +1,66 @@
+package com.eomcs.mylist.controller;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.eomcs.mylist.domain.Book;
+import com.eomcs.util.ArrayList;
+
+@RestController
+public class BookController {
+  ArrayList bookList = new ArrayList();
+
+  public BookController() {
+    System.out.println("BookController() 호출됨");
+  }
+
+  @RequestMapping("/book/list")
+  public Object list() {
+    return bookList.toArray(); 
+  }
+
+
+  @RequestMapping("/book/add")
+  public Object add(Book book) {
+
+    book.setListDate(nowTime());
+    bookList.add(book);
+    return bookList.size();
+  }
+
+
+  @RequestMapping("/book/get")
+  public Object get(int index) {
+    if (index < 0 || index >= bookList.size()) {
+      return "";
+    }
+    Book book = (Book) bookList.get(index);
+    return book;
+  }
+
+
+  @RequestMapping("/book/update")
+  public Object update(int index, Book book) {
+    if (index < 0 || index >= bookList.size()) {
+      return 0;
+    }
+    book.setListDate(nowTime());
+    return bookList.set(index, book) == null ? 0 : 1;
+  }
+
+
+  @RequestMapping("/book/delete")
+  public Object delete(int index) {
+    if (index < 0 || index >= bookList.size()) {
+      return 0;
+    }
+    return bookList.remove(index) == null ? 0 : 1;
+  }
+
+
+  String nowTime() {
+    return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss"));
+
+  }
+}
