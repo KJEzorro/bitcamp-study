7f2d60cb40f2f52614ffcaac0245b47aa53bb998
KJEzorro <gndlg91@gmail.com>
2022-01-10 17:08:37 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/io/FileReader2.java
+++ mylist-boot/app/src/main/java/com/eomcs/io/FileReader2.java
@@ -1,15 +1,16 @@
 package com.eomcs.io;
 
+import java.io.FileReader;
+
 // 기존 코드를 자신의 코드인양 사용하겠다고 선언한다.
-public class FileReader2 extends java.io.FileReader{
+public class FileReader2{
 
-  // super 클래스의 생성자는 바로 사용할 수 없다.
-  // 서브 클래스의 생성자를 통해 사용해야 한다.
+  //기존의 클래스 포함!
+  FileReader in;
+
   public FileReader2(String filename) throws Exception {
-
-    // 다음과 같이 수퍼 클래스의 생성자를 호출해야 한다.
-    super(filename);
-
+    // 기존 객체를 준비한다.
+    in = new FileReader(filename);
   }
 
   public String readLine() throws Exception {
@@ -17,8 +18,9 @@ public class FileReader2 extends java.io.FileReader{
     StringBuilder buf =  new StringBuilder();
     int c;
 
-    while ((c = this.read()) != -1) { // 파일에서 한 문자를 읽는다. 더이상 읽을 문자가 없으면 반복문을 종료한다.
-      if (c == '\n') {  // 만약 읽은 문자가 줄바꿈 명령이라면, 지금까지 버퍼에 저장한 문자를 리턴한다.
+    // 입력을 수행하기 위해 이 클래스가 포함하고 있는 기존 객체를 이용한다.
+    while ((c = in.read()) != -1) { 
+      if (c == '\n') {  
         return buf.toString();
       } else if(c == '\r') {
         // 무시! CR(Carrage Return; \r) 코드는 버퍼에 담지 말고 버린다.
@@ -29,5 +31,10 @@ public class FileReader2 extends java.io.FileReader{
     }
     return buf.toString();
   }
+  // 자원해제라는 요청이 들어오면 실제 일을 하는 객체에게 전달한다.
+  public void close() throws Exception {
+    in.close();
+  }
+
 
 }
--- mylist-boot/app/src/main/java/com/eomcs/io/FileWriter2.java
+++ mylist-boot/app/src/main/java/com/eomcs/io/FileWriter2.java
@@ -1,17 +1,27 @@
 package com.eomcs.io;
 
-// 상속 문법을 사용하여 기존의 FileWriter 클래스를 연결한다.
-// 그런 후 마치 자신의 코드인 양 사용한다.
-public class FileWriter2 extends java.io.FileWriter {
+import java.io.FileWriter;
 
-  // 이 클래스의 생성자를 통해 FileWriter 클래스의 생성자를 호출한다.
+// 위임 기법을 활용하여 기존 기능을 확장하기
+//
+public class FileWriter2 {
+  // 실제 파일로 데이터 출력을 실행할 객체를 준비한다.
+  FileWriter out;
+
+
+  // 이 클래스의 생성자를 통해 FileWriter 객체를 준비한다.
   public FileWriter2(String filename) throws Exception {
-    super(filename);
+    out = new FileWriter(filename);
   }
 
   // 한 줄 단위로 출력하는 기능을 추가한다.
   public void println(String str) throws Exception {
-    this.write(str + "\n");
+    // 실제 파일로 데이터를 출력하는 일은 FileWriter 객체에게 위임한다.
+    out.write(str + "\n");
   }
 
+  // 자원을 해제시키는 일은 실제 파일 출력을 수행하는 객체에게 넘긴다.
+  public void close() throws Exception {
+    out.close();
+  }
 }
