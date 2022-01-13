295256e0dff4d70870d9ae3ecb28fd56ca730dc5
KJEzorro <gndlg91@gmail.com>
2022-01-13 14:49:26 +0900

    .

---
+++ java-lang/app/src/main/java/com/eomcs/oop/ex05/x4/app2/CarTest2.java
@@ -0,0 +1,20 @@
+package com.eomcs.oop.ex05.x4.app2;
+
+
+// 트럭 만들기
+//
+// 1) 기존 코드를 상속하여 새 클래스를 만든 후 기능 덧붙이기
+
+public class CarTest2 {
+  public static void main(String[] args) {
+    // Car 클래스를 상속받아 트럭을 위한 덤프 기능을 추가한다.
+    // => dump() 메서드 추가.
+
+    Truck car = new Truck();
+    car.start();
+    car.run();
+    car.dump();
+    car.stop();
+
+  }
+}
