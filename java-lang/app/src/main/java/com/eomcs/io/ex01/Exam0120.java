3293e78a0561d7dd8f4cf287a79f1c4762235655
KJEzorro <gndlg91@gmail.com>
2022-01-14 19:19:33 +0900

    .

--- java-lang/app/src/main/java/com/eomcs/io/ex01/Exam0120.java
+++ java-lang/app/src/main/java/com/eomcs/io/ex01/Exam0120.java
@@ -10,7 +10,7 @@ public class Exam0120 {
     // => 파일이나 디렉토리 정보를 관리
     // => 파일이나 디렉토리를 생성,삭제,변경
 
-    // 상위 폴더 정보 조회
+    // ''상위 폴더'' 정보 조회
     // - ".." 으로 경로를 표시한다.
     //
     File currentDir = new File("./src/main/java/../../test/java");
