5102f192f605e23cee86f6d288c5f1bf6432f5bf
KJEzorro <gndlg91@gmail.com>
2022-01-21 14:49:49 +0900

    .

--- project-app1/app/src/main/java/com/eomcs/app1/App.java
+++ project-app1/app/src/main/java/com/eomcs/app1/App.java
@@ -3,27 +3,53 @@
  */
 package com.eomcs.app1;
 
+import java.util.Scanner;
+
 public class App {
 
   public static void main(String[] args) {
 
-    if (args[0].equals("add")) {
-      int v1 = Integer.parseInt(args[1]);
-      int v2 = Integer.parseInt(args[2]);
-      System.out.printf("%d + %d = %d", v1, v2, (v1 + v2));
+    Scanner keyScan = new Scanner(System.in);
+
+    while(true) {
+      System.out.print("> ");
+      String input = keyScan.nextLine();
+      if (input.equals("quit") || input.equals("exit")) {
+        break;
+      } else if (input.length() == 0) {
+        continue;
+      } else if (input.equals("help")) {
+        System.out.println("add   [값1] [값2]    더하기 계산을 수행한다.");
+        System.out.println("minus [값1] [값2]    빼기 계산을 수행한다.");
+        System.out.println("help                 도움말을 실행한다.");
+      } else {
+        String[] values = input.split(" ");
+        if (values[0].equals("add")) {
+          if (values.length != 3) {
+            System.out.println("명령어 입력 형식이 옳지 않습니다.");
+          } else {
+            int v1 = Integer.parseInt(values[1]);
+            int v2 = Integer.parseInt(values[2]);
+            System.out.printf("%s %d + %d = %d\n", values[0], v1, v2, (v1 + v2));
+          }
+        } else if (values[0].equals("minus")) {
+          if (values.length != 3) {
+            System.out.println("명령어 입력 형식이 옳지 않습니다.");
+          } else {
+            int v1 = Integer.parseInt(values[1]);
+            int v2 = Integer.parseInt(values[2]);
+            System.out.printf("%s %d - %d = %d\n", values[0], v1, v2, (v1 - v2));
+          }
+
+        } else {
+          System.out.println("지원하지 않는 연산자입니다.");
+        }
+      }
+
     }
-    if (args[0].equals("minus")) {
-      int v1 = Integer.parseInt(args[1]);
-      int v2 = Integer.parseInt(args[2]);
-      System.out.printf("%d - %d = %d", v1, v2, (v1 - v2));
-    }
-    if (args[0] == "") {
-      System.out.println("사용법: ");
-      System.out.println("App [명령] [값1] [값2]");
-      System.out.println("명령: ");
-      System.out.println("add [값1] [값2] 더하기 계산을 수행한다.   예) App add 100 200");
-      System.out.println("minus [값1] [값2] 빼기 계산을 수행한다.   예) App minus 100 200");
-    }
+
+    keyScan.close();
+
 
 
   }
