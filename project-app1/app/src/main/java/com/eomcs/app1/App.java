e92f49fae1cc1843fa242be43a6eb636a30f1d11
KJEzorro <gndlg91@gmail.com>
2022-01-21 12:23:52 +0900

    .

--- project-app1/app/src/main/java/com/eomcs/app1/App.java
+++ project-app1/app/src/main/java/com/eomcs/app1/App.java
@@ -4,11 +4,27 @@
 package com.eomcs.app1;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+
+  public static void main(String[] args) {
+
+    if (args[0].equals("add")) {
+      int v1 = Integer.parseInt(args[1]);
+      int v2 = Integer.parseInt(args[2]);
+      System.out.printf("%d + %d = %d", v1, v2, (v1 + v2));
+    }
+    if (args[0].equals("minus")) {
+      int v1 = Integer.parseInt(args[1]);
+      int v2 = Integer.parseInt(args[2]);
+      System.out.printf("%d - %d = %d", v1, v2, (v1 - v2));
+    }
+    if (args[0] == "") {
+      System.out.println("사용법: ");
+      System.out.println("App [명령] [값1] [값2]");
+      System.out.println("명령: ");
+      System.out.println("add [값1] [값2] 더하기 계산을 수행한다.   예) App add 100 200");
+      System.out.println("minus [값1] [값2] 빼기 계산을 수행한다.   예) App minus 100 200");
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+
+  }
 }
--- project-app1/app/src/test/java/com/eomcs/app1/AppTest.java
+++ project-app1/app/src/test/java/com/eomcs/app1/AppTest.java
@@ -4,11 +4,10 @@
 package com.eomcs.app1;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
+  @Test public void appHasAGreeting() {
+    App classUnderTest = new App();
+    //        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  }
 }
