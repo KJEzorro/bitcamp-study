74037cffd2d19283da043daaeb52cf41d0717ddb
KJEzorro <gndlg91@gmail.com>
2022-02-04 10:43:17 +0900

    .

--- project-app1/app/src/main/java/com/eomcs/app1/App.java
+++ project-app1/app/src/main/java/com/eomcs/app1/App.java
@@ -10,30 +10,40 @@ public class App {
     Console console = new Console();
     CommandHandler commandHandler = new CommandHandler();
 
-
     while(true) {
 
       Command command = console.prompt();
 
-      if (command.getName().equals("quit") || command.getName().equals("exit")) {
-        break;
+      try {
+        // 예외가 발생할 가능성이 있는 코드는 try 블록 안에 가둔다.
+        if (command.getName().equals("quit") || command.getName().equals("exit")) {
+          break;
 
-      } else if (command.getName().equals("")) {
-        continue;
+        } else if (command.getName().equals("")) {
+          continue;
 
-      } else if (command.getName().equals("help")) {
-        commandHandler.doHelp();
+        } else if (command.getName().equals("help")) {
+          commandHandler.doHelp();
 
-      } else if (command.getName().equals("add")) {
-        commandHandler.doAdd(command);
+        } else if (command.getName().equals("add")) {
+          commandHandler.doAdd(command);
 
-      } else if (command.getName().equals("minus")) {
-        commandHandler.doMinus(command);
+        } else if (command.getName().equals("minus")) {
+          commandHandler.doMinus(command);
 
-      } else {
-        System.out.println("지원하지 않는 연산자입니다.");
+        } else if (command.getName().equals("divide")) {
+          commandHandler.doDivide(command);
+
+        }else {
+          System.out.println("지원하지 않는 연산자입니다.");
+        }
+      } catch (Exception e) {
+        // try 블록에서 발생한 예외는 여기에서 잡는다.
+        // 예외에 대한 적절한 조치를 취하는 코드를 둔다.
+        System.out.println("명령어 처리 중 오류 발생");
+
       }
-    }
+    } 
     console.close();
   }
 }
--- project-app1/app/src/main/java/com/eomcs/app1/CommandHandler.java
+++ project-app1/app/src/main/java/com/eomcs/app1/CommandHandler.java
@@ -3,8 +3,9 @@ package com.eomcs.app1;
 public class CommandHandler {
 
   void doHelp() {
-    System.out.println("add   [값1] [값2]    더하기 계산을 수행한다.");
-    System.out.println("minus [값1] [값2]    빼기 계산을 수행한다.");
+    System.out.println("add    [값1] [값2]    더하기 계산을 수행한다.");
+    System.out.println("minus  [값1] [값2]    빼기 계산을 수행한다.");
+    System.out.println("divide [값1] [값2]    나누기 계산을 수행한다.");
     System.out.println("help                 도움말을 실행한다.");
   }
 
@@ -31,4 +32,31 @@ public class CommandHandler {
       System.out.printf("%s %d - %d = %d\n", command.name, v1, v2, (v1 - v2));
     }
   }
+
+  void doDivide(Command command) {
+    if (command.getParamSize() != 2) {
+      System.out.println("divide 명령어 입력 형식이 옳지 않습니다.");
+      System.out.println("형식: divide 값1 값2");
+      System.out.println("예) divide 100 200");
+    } else {
+      int v1 = command.getInt(0);
+      int v2 = command.getInt(1);
+      System.out.printf("%s %d / %d = %d\n", command.name, v1, v2, (v1 / v2));
+    }
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
