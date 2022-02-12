d24567100576e4ad8bd878ca5c60bd1f8fdcfd97
KJEzorro <gndlg91@gmail.com>
2022-02-12 15:59:29 +0900

    .

--- java-lang/app/src/main/java/com/eomcs/net/ex12/swing/ChatClient.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex12/swing/ChatClient.java
@@ -6,8 +6,12 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -16,11 +20,23 @@ import javax.swing.UIManager;
 public class ChatClient extends JFrame {
   private static final long serialVersionUID = 1L;
 
+  Socket socket;
+  DataInputStream in;
+  DataOutputStream out;
+
+  JTextField addressTf = new JTextField(30);
+  JTextField portTf = new JTextField(4);
+  JTextArea messageListTa = new JTextArea();
+  JTextField messageTf = new JTextField(40);
+
   public ChatClient() {
     super("계산기2");
     addWindowListener(new WindowAdapter() {
       @Override
       public void windowClosing(WindowEvent e) {
+        try {in.close();} catch (Exception ex) {}
+        try {out.close();} catch (Exception ex) {}
+        try {socket.close();} catch (Exception ex) {}
         // 윈도우의 x 버튼을 눌렀을 때
         System.exit(0);
       }
@@ -28,16 +44,10 @@ public class ChatClient extends JFrame {
     setSize(700, 600);
 
     Container contentPane = this.getContentPane();
-
     JPanel topPanel = new JPanel();
     topPanel.setLayout(new FlowLayout(FlowLayout.RIGHT)); // 기본 레이아웃 관리자를 교체
-
-    JTextField addressTf = new JTextField(30);
     topPanel.add(addressTf);
-
-    JTextField portTf = new JTextField(4);
     topPanel.add(portTf);
-
     JButton connectBtn = new JButton("연결");
 
     //  1) 로컬 클래스
@@ -62,23 +72,16 @@ public class ChatClient extends JFrame {
     connectBtn.addActionListener(this::connectChatServer);
 
     topPanel.add(connectBtn);
-
     contentPane.add(topPanel, BorderLayout.NORTH);
-
-
-    JTextArea messageListTa = new JTextArea();
     contentPane.add(messageListTa, BorderLayout.CENTER);
-
 
     JPanel bottomPanel = new JPanel();
     bottomPanel.setLayout(new FlowLayout(FlowLayout.RIGHT)); // 기본 레이아웃 관리자를 교체
-
-    JTextField messageTf = new JTextField(40);
     bottomPanel.add(messageTf);
 
     JButton sendBtn = new JButton("보내기");
+    sendBtn.addActionListener(this::sendMessage);
     bottomPanel.add(sendBtn);
-
     contentPane.add(bottomPanel, BorderLayout.SOUTH);
 
     setVisible(true);
@@ -99,12 +102,38 @@ public class ChatClient extends JFrame {
 
   public void connectChatServer(ActionEvent e) {
     System.out.println("서버에 연결하기");
+
+    try {
+      socket = new Socket(
+          addressTf.getText(),
+          Integer.parseInt(portTf.getText()));
+
+      in = new DataInputStream(socket.getInputStream());
+      out = new DataOutputStream(socket.getOutputStream());
+
+
+    } catch (Exception ex) {
+      JOptionPane.showMessageDialog(this, "서버에 연결 중 오류", "통신 오류", JOptionPane.ERROR_MESSAGE);
+    }
   }
 
   public void sendMessage(ActionEvent e) {
     System.out.println("메시지 보내기 ");
+
+    try {
+      out.writeUTF(messageTf.getText());
+    } catch (Exception ex) {
+      JOptionPane.showMessageDialog(this, "메시지 전송 오류", "통신 오류", JOptionPane.ERROR_MESSAGE);
+    }
   }
 }
+
+
+
+
+
+
+
 
 
 
