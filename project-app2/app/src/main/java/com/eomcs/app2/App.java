1041efc9e7866727a437622313e264c5117d130b
KJEzorro <gndlg91@gmail.com>
2022-02-16 12:23:27 +0900

    .

--- project-app2/app/src/main/java/com/eomcs/app2/App.java
+++ project-app2/app/src/main/java/com/eomcs/app2/App.java
@@ -3,11 +3,13 @@
  */
 package com.eomcs.app2;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
 
   Scanner keyScan = new Scanner(System.in);
+  ArrayList<Score> scores = new ArrayList<>();
 
   public static void main(String[] args) {
     new App().service();
@@ -16,7 +18,7 @@ public class App {
   public void service() {
     while (true) {
       printMenu();
-      String input = prompt();
+      String input = promptString("명령> ");
 
       if (checkQuit(input)) {
         break;
@@ -52,9 +54,13 @@ public class App {
     System.out.println("5. 삭제");
   }
 
-  private String prompt() {
-    System.out.print("명령> ");
+  private String promptString(String title) {
+    System.out.print(title);
     return keyScan.nextLine();
+  }
+
+  private int promptInt(String title) {
+    return Integer.parseInt(promptString(title));
   }
 
   private boolean checkQuit(String input) {
@@ -62,30 +68,62 @@ public class App {
   }
 
   private void createScore() {
-    System.out.print("이름? ");
-    String name = keyScan.nextLine();
+    Score score = new Score();
+    score.setName(promptString("이름? "));
+    score.setKor(promptInt("국어? "));
+    score.setEng(promptInt("영어? "));
+    score.setMath(promptInt("수학? "));
 
-    System.out.print("국어? ");
-    int kor = Integer.parseInt(keyScan.nextLine());
-
-    System.out.print("영어? ");
-    int eng = Integer.parseInt(keyScan.nextLine());
-
-    System.out.print("수학? ");
-    int math = Integer.parseInt(keyScan.nextLine());
-
+    scores.add(score);
   }
 
   private void listScore() {
+    int count = 0;
+    for (Score score : scores) {
+      System.out.printf("%d: %s, %d, %d, %d, %d, %.1f\n",
+          count++,
+          score.getName(),
+          score.getKor(),
+          score.getEng(),
+          score.getMath(),
+          score.getSum(),
+          score.getAverage());
+    }
 
   }
 
   private void detailScore() {
+    int no = promptInt("번호? ");
+    if (no < 0 || no >= scores.size()) {
+      System.out.println("번호가 유효하지 않습니다.");
+      return;
+    }
 
+    Score score = scores.get(no);
+    System.out.printf("이름: %s\n", score.getName());
+    System.out.printf("국어: %d\n", score.getKor());
+    System.out.printf("영어: %d\n", score.getEng());
+    System.out.printf("수학: %d\n", score.getMath());
+    System.out.printf("합계: %d\n", score.getSum());
+    System.out.printf("평균: %.1f\n", score.getAverage());
   }
 
   private void updateScore() {
+    int no = promptInt("번호? ");
+    if (no < 0 || no >= scores.size()) {
+      System.out.println("번호가 유효하지 않습니다.");
+      return;
+    }
 
+    Score old = scores.get(no);
+
+    Score score = new Score();
+    score.setName(promptString(String.format("이름(%s)?", old.getName())));
+    score.setKor(promptInt(String.format("국어(%d)?", old.getKor())));
+    score.setEng(promptInt(String.format("영어(%d)?", old.getEng())));
+    score.setMath(promptInt(String.format("수학(%d)?", old.getMath())));
+
+    scores.set(no, score);
   }
 
   private void deleteScore() {
---
+++ project-app2/app/src/main/java/com/eomcs/app2/Score.java
@@ -0,0 +1,62 @@
+package com.eomcs.app2;
+
+public class Score {
+  String name;
+  int kor;
+  int eng;
+  int math;
+  int sum;
+  float average;
+
+  private void compute() {
+    sum = kor + eng + math;
+    average = sum / 3f;
+  }
+
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public int getKor() {
+    return kor;
+  }
+  public void setKor(int kor) {
+    this.kor = kor;
+    compute();
+
+  }
+  public int getEng() {
+    return eng;
+  }
+  public void setEng(int eng) {
+    this.eng = eng;
+    compute();
+  }
+  public int getMath() {
+    return math;
+  }
+  public void setMath(int math) {
+    this.math = math;
+    compute();
+  }
+  public int getSum() {
+    return sum;
+  }
+  public float getAverage() {
+    return average;
+  }
+
+  @Override
+  public String toString() {
+    return "Score [name=" + name + ", kor=" + kor + ", eng=" + eng + ", math=" + math + ", sum="
+        + sum + ", average=" + average + "]";
+  }
+
+
+
+
+
+
+}
