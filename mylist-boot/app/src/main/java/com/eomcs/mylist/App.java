b90db137b3dad0e6370533deb1a2dbce865bb5bb
KJEzorro <gndlg91@gmail.com>
2022-03-11 12:34:03 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class App {
 
-  // DB 커넥션풀 준비
-  public static DataSource datasource;
-
-  static {
+  // Spring 프레임워크(IoC = Object pool(객체풀)) 에서 객체를 생성한 후 보관하도록 만드는 방법
+  @Bean // => 다음 메서드를 호출한 후 이 메서드가 리턴한 값을 스프링 부트에 보관하라고 지시하는 애노테이션
+  public DataSource createDataSource() {
+    System.out.println("createDataSource() 호출됨!!!!");
     try {
-      datasource = new DriverManagerDataSource(
-          "jdbc:mariadb://localhost:3306/studydb?user=study&password=1111");
+      DriverManagerDataSource connectionPool = new DriverManagerDataSource();
+      connectionPool.setDriverClassName("org.mariadb.jdbc.Driver");
+      connectionPool.setUrl("jdbc:mariadb://localhost:3306/studydb");
+      connectionPool.setUsername("study");
+      connectionPool.setPassword("1111");
+      return connectionPool;
 
     } catch (Exception e) {
-      System.out.println("DB 연결 중 오류 발생");
-      e.printStackTrace();
+      throw new RuntimeException(e);
     }
   }
 
--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/mariadb/BoardDaoImpl.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/mariadb/BoardDaoImpl.java
@@ -1,11 +1,13 @@
 package com.eomcs.mylist.dao.mariadb;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import com.eomcs.mylist.App;
 import com.eomcs.mylist.dao.BoardDao;
 import com.eomcs.mylist.dao.DaoException;
 import com.eomcs.mylist.domain.Board;
@@ -16,14 +18,19 @@ import com.eomcs.mylist.domain.Board;
 @Repository   
 public class BoardDaoImpl implements BoardDao {
 
+  @Autowired // => 스프링 부트가 보관하고 있는 객체 중에서 다음 타입의 객체가 있다면 주입해 줄 것을 지시하는 애노테이션.
+  DataSource dataSource;
+
   public BoardDaoImpl() {
     System.out.println("BoardDao 객체 생성");
   }
 
   @Override
   public int countAll() {
-    try (PreparedStatement stmt = App.con.prepareStatement( 
-        "select count(*) from ml_board");
+    try (Connection con = dataSource.getConnection(); 
+        // DataSource에서 얻은 커넥션 객체는 close() 할 때 연결을 끊는 것이 아니라 DataSource에 반납된다. 
+        PreparedStatement stmt = con.prepareStatement( 
+            "select count(*) from ml_board");
         ResultSet rs = stmt.executeQuery()) {
 
       rs.next();
@@ -35,8 +42,9 @@ public class BoardDaoImpl implements BoardDao {
 
   @Override
   public List<Board> findAll() {
-    try (PreparedStatement stmt = App.con.prepareStatement( 
-        "select board_no,title,created_date,view_count from ml_board order by board_no desc");
+    try (Connection con = dataSource.getConnection();
+        PreparedStatement stmt = con.prepareStatement( 
+            "select board_no,title,created_date,view_count from ml_board order by board_no desc");
         ResultSet rs = stmt.executeQuery()) {
 
       ArrayList<Board> arr = new ArrayList<>();
@@ -56,8 +64,9 @@ public class BoardDaoImpl implements BoardDao {
 
   @Override
   public int insert(Board board) {
-    try (PreparedStatement stmt =
-        App.con.prepareStatement("insert into ml_board(title,content) values(?,?)");) {
+    try (Connection con = dataSource.getConnection();
+        PreparedStatement stmt =
+            con.prepareStatement("insert into ml_board(title,content) values(?,?)");) {
 
       stmt.setString(1, board.getTitle());
       stmt.setString(2, board.getContent());
@@ -70,8 +79,9 @@ public class BoardDaoImpl implements BoardDao {
 
   @Override
   public Board findByNo(int no) {
-    try (PreparedStatement stmt = App.con.prepareStatement(
-        "select board_no,title,content,created_date,view_count from ml_board where board_no=?")) {
+    try (Connection con = dataSource.getConnection();
+        PreparedStatement stmt = con.prepareStatement(
+            "select board_no,title,content,created_date,view_count from ml_board where board_no=?")) {
 
       stmt.setInt(1, no);
 
@@ -95,8 +105,9 @@ public class BoardDaoImpl implements BoardDao {
 
   @Override
   public int update(Board board) {
-    try (PreparedStatement stmt = App.con.prepareStatement( //
-        "update ml_board set title=?,content=? where board_no=?")) {
+    try (Connection con = dataSource.getConnection();
+        PreparedStatement stmt = con.prepareStatement( //
+            "update ml_board set title=?,content=? where board_no=?")) {
 
       stmt.setString(1, board.getTitle());
       stmt.setString(2, board.getContent());
@@ -110,8 +121,9 @@ public class BoardDaoImpl implements BoardDao {
 
   @Override
   public int delete(int no) {
-    try (PreparedStatement stmt = App.con.prepareStatement(
-        "delete from ml_board where board_no=?")) {
+    try (Connection con = dataSource.getConnection();
+        PreparedStatement stmt = con.prepareStatement(
+            "delete from ml_board where board_no=?")) {
 
       stmt.setInt(1, no);
       return stmt.executeUpdate();
@@ -122,8 +134,9 @@ public class BoardDaoImpl implements BoardDao {
 
   @Override
   public int increaseViewCount(int no) {
-    try (PreparedStatement stmt = App.con.prepareStatement( //
-        "update ml_board set view_count=view_count + 1 where board_no=?")) {
+    try (Connection con = dataSource.getConnection();
+        PreparedStatement stmt = con.prepareStatement( //
+            "update ml_board set view_count=view_count + 1 where board_no=?")) {
 
       stmt.setInt(1, no);
       return stmt.executeUpdate();
