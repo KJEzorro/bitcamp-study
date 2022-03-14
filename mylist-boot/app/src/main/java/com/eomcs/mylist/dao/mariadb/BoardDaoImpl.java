73c187c186ca61aa461641e975629f3045b7487d
KJEzorro <gndlg91@gmail.com>
2022-03-14 12:42:38 +0900

    >

--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/mariadb/BoardDaoImpl.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/mariadb/BoardDaoImpl.java
@@ -1,16 +1,11 @@
 package com.eomcs.mylist.dao.mariadb;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.List;
-import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import com.eomcs.mylist.dao.BoardDao;
-import com.eomcs.mylist.dao.DaoException;
 import com.eomcs.mylist.domain.Board;
 // @Repository
 // - 클래스에 이 애너테이션을 붙여 표시해 두면, Spring Boot가 실행될 때 이 클래스의 객체를 자동 생성한다.
@@ -18,9 +13,6 @@ import com.eomcs.mylist.domain.Board;
 
 @Repository   
 public class BoardDaoImpl implements BoardDao {
-
-  @Autowired // => 스프링 부트가 보관하고 있는 객체 중에서 다음 타입의 객체가 있다면 주입해 줄 것을 지시하는 애노테이션.
-  DataSource dataSource;
 
   @Autowired
   SqlSessionFactory sqlSessionFactory; // => Mybatis: 제공하는 SQL 실행 도구를 만들어 주는 객체.
@@ -31,16 +23,8 @@ public class BoardDaoImpl implements BoardDao {
 
   @Override
   public int countAll() {
-    try (Connection con = dataSource.getConnection(); 
-        // DataSource에서 얻은 커넥션 객체는 close() 할 때 연결을 끊는 것이 아니라 DataSource에 반납된다. 
-        PreparedStatement stmt = con.prepareStatement( 
-            "select count(*) from ml_board");
-        ResultSet rs = stmt.executeQuery()) {
-
-      rs.next();
-      return rs.getInt(1);
-    } catch (Exception e) {
-      throw new DaoException(e);
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) { // SQL을 실행시켜주는 도구를 준비
+      return sqlSession.selectOne("BoardDao.sql5");
     }
   }
 
@@ -53,84 +37,36 @@ public class BoardDaoImpl implements BoardDao {
 
   @Override
   public int insert(Board board) {
-    try (Connection con = dataSource.getConnection();
-        PreparedStatement stmt =
-            con.prepareStatement("insert into ml_board(title,content) values(?,?)");) {
-
-      stmt.setString(1, board.getTitle());
-      stmt.setString(2, board.getContent());
-
-      return stmt.executeUpdate();
-    } catch (Exception e) {
-      throw new DaoException(e);
+    try (SqlSession sqlSession = sqlSessionFactory.openSession();) { // SQL을 실행시켜주는 도구를 준비
+      return sqlSession.insert("BoardDao.sql3", board);
     }
   }
 
   @Override
   public Board findByNo(int no) {
-    try (Connection con = dataSource.getConnection();
-        PreparedStatement stmt = con.prepareStatement(
-            "select board_no,title,content,created_date,view_count from ml_board where board_no=?")) {
-
-      stmt.setInt(1, no);
-
-      try (ResultSet rs = stmt.executeQuery()) {
-        if (rs.next()) {
-          Board board = new Board();
-          board.setNo(rs.getInt("board_no"));
-          board.setTitle(rs.getString("title"));
-          board.setContent(rs.getString("content"));
-          board.setCreatedDate(rs.getDate("created_date"));
-          board.setViewCount(rs.getInt("view_count"));
-          return board;
-        } else {
-          return null;
-        }
-      }
-    } catch (Exception e) {
-      throw new DaoException(e);
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) { // SQL을 실행시켜주는 도구를 준비
+      return sqlSession.selectOne("BoardDao.sql2", no);
     }
   }
 
   @Override
   public int update(Board board) {
-    try (Connection con = dataSource.getConnection();
-        PreparedStatement stmt = con.prepareStatement( //
-            "update ml_board set title=?,content=? where board_no=?")) {
-
-      stmt.setString(1, board.getTitle());
-      stmt.setString(2, board.getContent());
-      stmt.setInt(3, board.getNo());
-
-      return stmt.executeUpdate();
-    } catch (Exception e) {
-      throw new DaoException(e);
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) { // SQL을 실행시켜주는 도구를 준비
+      return sqlSession.selectOne("BoardDao.sql4", board);
     }
   }
 
   @Override
   public int delete(int no) {
-    try (Connection con = dataSource.getConnection();
-        PreparedStatement stmt = con.prepareStatement(
-            "delete from ml_board where board_no=?")) {
-
-      stmt.setInt(1, no);
-      return stmt.executeUpdate();
-    } catch (Exception e) {
-      throw new DaoException(e);
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) { // SQL을 실행시켜주는 도구를 준비
+      return sqlSession.delete("BoardDao.sql6", no);
     }
   }
 
   @Override
   public int increaseViewCount(int no) {
-    try (Connection con = dataSource.getConnection();
-        PreparedStatement stmt = con.prepareStatement( //
-            "update ml_board set view_count=view_count + 1 where board_no=?")) {
-
-      stmt.setInt(1, no);
-      return stmt.executeUpdate();
-    } catch (Exception e) {
-      throw new DaoException(e);
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) { // SQL을 실행시켜주는 도구를 준비
+      return sqlSession.update("BoardDao.sql7", no);
     }
   }  
 }
