2eaae0827e9f9297e325953d8d89c896ef9e4c66
KJEzorro <gndlg91@gmail.com>
2022-03-16 14:50:09 +0900

    .

---
+++ web/app/src/main/resources/static/css/testx.js
@@ -0,0 +1,8 @@
+
+
+
+var btn = document.getElementById("closeBtn");
+    btn.addEventListener('click', function() {
+      var adDiv = document.getElementById("ad");
+      adDiv.style.display = "none";
+    });
