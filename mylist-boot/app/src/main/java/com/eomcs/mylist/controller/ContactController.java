142014680b0e23665f0d3d50cef60d48bbb805e0
KJEzorro <gndlg91@gmail.com>
2022-03-21 19:47:53 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
@@ -41,7 +41,7 @@ public class ContactController {
   public Object add(Contact contact, String[] tel) {
     contactDao.insert(contact);
     for (int i = 0; i < tel.length; i++) {
-      String[] value = tel[i].split(","); 
+      String[] value = tel[i].split("_"); 
       if (value[1].length() == 0) {
         continue;
       }
@@ -66,7 +66,7 @@ public class ContactController {
     if (count > 0) {
       contactDao.deleteTelByContactNo(contact.getNo());
       for (int i = 0; i < tel.length; i++) {
-        String[] value = tel[i].split(","); 
+        String[] value = tel[i].split("_"); 
         if (value[1].length() == 0) {
           continue;
         } 
