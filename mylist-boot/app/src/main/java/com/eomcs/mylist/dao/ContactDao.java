1626a57fa462e99df271ed73b39d9a02a5bd6f17
KJEzorro <gndlg91@gmail.com>
2022-03-22 12:24:48 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/ContactDao.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/ContactDao.java
@@ -2,6 +2,7 @@ package com.eomcs.mylist.dao;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import com.eomcs.mylist.domain.Contact;
 import com.eomcs.mylist.domain.ContactTel;
 
@@ -31,6 +32,8 @@ public interface ContactDao {
   List<ContactTel> findTelByContactNo(int contactNo);
 
   int insertTel(ContactTel tel);
+
+  int insertTels(@Param("contactNo") int contactNo, @Param("tels") List<ContactTel> tels);
 
   int updateTel(ContactTel tel);
 
--- mylist-boot/app/src/main/java/com/eomcs/mylist/service/impl/DefaultContactService.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/service/impl/DefaultContactService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.eomcs.mylist.dao.ContactDao;
 import com.eomcs.mylist.domain.Contact;
-import com.eomcs.mylist.domain.ContactTel;
 import com.eomcs.mylist.service.ContactService;
 
 @Service
@@ -20,29 +19,19 @@ public class DefaultContactService implements ContactService {
   @Transactional // 다음 메서드는 트랜잭션 안에서 실행하도록 설정한다.
   public int add(Contact contact) {
     contactDao.insert(contact);
-    for (ContactTel tel : contact.getTels()) {
-      tel.setContactNo(contact.getNo()); // 전화번호 입력 전에 자동 생성된 연락처 번호를 설정한다.
-      contactDao.insertTel(tel);
-    }
+    contactDao.insertTels(contact.getNo(), contact.getTels());
     return 1;
   }
 
   @Override
   public List<Contact> list() {
-    List<Contact> contacts = contactDao.findAll();
-    for (Contact contact : contacts) {
-      contact.setTels(contactDao.findTelByContactNo(contact.getNo()));
-    }
-    return contacts;
+    return contactDao.findAll();
+
   }
 
   @Override
   public Contact get(int no) {
-    Contact contact = contactDao.findByNo(no);
-    if (contact != null) {
-      contact.setTels(contactDao.findTelByContactNo(no));
-    }
-    return contact;
+    return contactDao.findByNo(no);
   }
 
   @Override
@@ -51,9 +40,7 @@ public class DefaultContactService implements ContactService {
     int count = contactDao.update(contact);
     if (count > 0) {
       contactDao.deleteTelByContactNo(contact.getNo()); // 전화번호 변경 전에 기존 전화번호를 모두 삭제한다.
-      for (ContactTel tel : contact.getTels()) {
-        contactDao.insertTel(tel); // 전화번호 객체 안에 이미 연락처 번호가 저장되어 있다.
-      }
+      contactDao.insertTels(contact.getNo(), contact.getTels()); // 전화번호 객체 안에 이미 연락처 번호가 저장되어 있다.
     }
     return count;
   }
