c7cc4c38ee008035cc3e7a153b1d662c5cae462a
KJEzorro <gndlg91@gmail.com>
2022-04-01 14:05:42 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/MemberController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.eomcs.mylist.controller;
 import static com.eomcs.mylist.controller.ResultMap.FAIL;
 import static com.eomcs.mylist.controller.ResultMap.SUCCESS;
 import java.util.Map;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +30,7 @@ public class MemberController {
   }
 
   @RequestMapping("/member/signin")
-  public Object signIn(String email, String password, HttpSession session) {
+  public Object signIn(String email, String password, boolean saveEmail, HttpServletResponse response,  HttpSession session) {
     Member loginUser =  memberService.get(email, password);
     if (loginUser == null) {
       return "fail";
@@ -37,6 +39,17 @@ public class MemberController {
     // 로그인이 성공하면,
     // 다른 요청을 처리할 때 로그인 회원의 정보를 사용할 수 있도록 세션에 보관한다.
     session.setAttribute("loginUser", loginUser);
+
+    Cookie cookie = null;
+    if (saveEmail) {
+      // 클라이언트로 보낼 데이터인 쿠키에 이메일을 저장한다.
+      cookie = new Cookie("userEmail", email);
+    } else {
+      cookie = new Cookie("userEmail", "");
+      cookie.setMaxAge(0);  // 클라이언트에게 해당 이름의 쿠키를 삭제하도록 요구한다.
+    }
+    response.addCookie(cookie);  // 응답할 때 쿠키 정보를 응답헤더에 포함시킨다.
+
     return "success";
   }
 
